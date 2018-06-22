d004e71f0fd761ec1f910d3d00620471d972f336
admin <yangxinbin@hotmail.com>
2018-06-22 19:55:26 +0800

    巡检

--- app/src/main/java/com/mango/leo/dcom/rotor/XunJianActivity.java
+++ app/src/main/java/com/mango/leo/dcom/rotor/XunJianActivity.java
@@ -1,15 +1,47 @@
 package com.mango.leo.dcom.rotor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.mango.leo.dcom.R;
+import com.mango.leo.dcom.scan.EditScanActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class XunJianActivity extends AppCompatActivity {
+
+    @Bind(R.id.imageView_b)
+    ImageView imageViewB;
+    @Bind(R.id.imageView_p)
+    ImageView imageViewP;
+    @Bind(R.id.imageView_delete)
+    ImageView imageViewDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xun_jian);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.imageView_b, R.id.imageView_p, R.id.imageView_delete})
+    public void onViewClicked(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.imageView_b:
+                intent = new Intent(this, BasicActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.imageView_p:
+                break;
+            case R.id.imageView_delete:
+                break;
+        }
     }
 }
