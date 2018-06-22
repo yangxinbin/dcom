4c31a3d8f0f2127d96824f1701debb3cb913e358
admin <yangxinbin@hotmail.com>
2018-06-22 19:51:49 +0800

    巡检

--- app/src/main/java/com/mango/leo/dcom/rotor/BasicActivity.java
+++ app/src/main/java/com/mango/leo/dcom/rotor/BasicActivity.java
@@ -1,13 +1,20 @@
 package com.mango.leo.dcom.rotor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mango.leo.dcom.DcomActivity;
 import com.mango.leo.dcom.R;
+import com.mango.leo.dcom.scan.EditScanActivity;
 import com.mango.leo.dcom.util.CircleProgressBar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class BasicActivity extends AppCompatActivity {
 
@@ -18,6 +25,12 @@ public class BasicActivity extends AppCompatActivity {
     CircleProgressBar circleBar1;
     @Bind(R.id.circle_bar2)
     CircleProgressBar circleBar2;
+    @Bind(R.id.imageView_back)
+    ImageView imageViewBack;
+    @Bind(R.id.textView_xunjian)
+    TextView textViewXunjian;
+    @Bind(R.id.imageView_edit)
+    ImageView imageViewEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +41,24 @@ public class BasicActivity extends AppCompatActivity {
         circleBar1.setProgress(55);//circle百分比
         circleBar2.setProgress(85);//circle百分比
 
+    }
+
+    @OnClick({R.id.imageView_back, R.id.textView_xunjian, R.id.imageView_edit})
+    public void onViewClicked(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.imageView_back:
+                intent = new Intent(this, EditScanActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.textView_xunjian:
+                intent = new Intent(this, XunJianActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.imageView_edit:
+                break;
+        }
     }
 }
