1aac90484f69f3fa823dc9fd71fd7434b02afdbf
admin <yangxinbin@hotmail.com>
2018-07-25 10:55:59 +0800

    Change修改刷新加载样式，目前用框架默认自带带时间的，如果要自定义使用自己的类ClassicsHeader类。

--- app/src/main/java/com/mango/leo/dcom/change/activity/AllChangeFragment.java
+++ app/src/main/java/com/mango/leo/dcom/change/activity/AllChangeFragment.java
@@ -3,30 +3,30 @@ package com.mango.leo.dcom.change.activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.mango.leo.dcom.DcomActivity;
 import com.mango.leo.dcom.R;
-import com.mango.leo.dcom.change.adapter.ChangeAdapter;
+import com.mango.leo.dcom.change.adapter.SmartChangeAdapter;
 import com.mango.leo.dcom.change.bean.ChangeBean;
 import com.mango.leo.dcom.change.bean.ListChangeBean;
 import com.mango.leo.dcom.change.presenter.ChangePresenter;
 import com.mango.leo.dcom.change.presenter.ChangePresenterImpl;
 import com.mango.leo.dcom.change.view.ChangeView;
 import com.mango.leo.dcom.util.AppUtils;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,45 +43,89 @@ class AllChangeFragment extends Fragment implements ChangeView {
     @Bind(R.id.recycle)
     RecyclerView recycle;
     @Bind(R.id.refresh)
-    SwipeRefreshLayout refresh;
+    SmartRefreshLayout refresh;
     private LinearLayoutManager mLayoutManager;
-    private ChangeAdapter adapter;
+    private SmartChangeAdapter adapter;
     private ArrayList<ListChangeBean> mData, mDataAll;
     private int page = 0;
     private ChangePresenter changePresenter;
     private final int TYPE = 1;
     private ChangeBean changeBean;
+    private boolean isFirstEnter = true;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.recycle_view, container, false);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.smart_recycle, container, false);
         changePresenter = new ChangePresenterImpl(this);
         ButterKnife.bind(this, view);
         initRecycle();
-        initHeader();
-        initSwipeRefreshLayout();
         changeBean = new ChangeBean();
         changePresenter.visitProjects(getActivity(), TYPE, changeBean, page);
+        refreshAndLoadMore();
         return view;
     }
+    private void refreshAndLoadMore() {
+        refresh.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull final RefreshLayout refreshLayout) {
+                refreshLayout.getLayout().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mDataAll != null) {
+                            mDataAll.clear();
+                        }
+                        if (mData != null) {
+                            mData.clear();
+                        }
+                        page = 0;
+                        Log.v("zzzzzzzzz", "-------onRefresh-------" + page);
+                        changePresenter.visitProjects(getActivity(), TYPE, changeBean, page);
+                        refreshLayout.finishRefresh();
+                    }
+                }, 2000);
+            }
+        });
+        refresh.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull final RefreshLayout refreshLayout) {
+                refreshLayout.getLayout().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        page++;
+                        Log.v("zzzzzzzzz", "-------onLoadMore-------" + page);
+                        changePresenter.visitProjects(getActivity(), TYPE, changeBean, page);
+                        refreshLayout.finishLoadMore();
 
+                    }
+                }, 2000);
+            }
+        });
+        refresh.setRefreshHeader(new ClassicsHeader(getActivity()));
+        refresh.setHeaderHeight(60);
+
+        //触发自动刷新
+        if (isFirstEnter) {
+            isFirstEnter = false;
+            //refresh.autoRefresh();
+        } else {
+            //mAdapter.refresh(initData());
+        }
+    }
     private void initRecycle() {
         mLayoutManager = new LinearLayoutManager(getActivity());
         recycle.setLayoutManager(mLayoutManager);
         recycle.setItemAnimator(new DefaultItemAnimator());//设置默认动画
-        adapter = new ChangeAdapter(getActivity().getApplicationContext());
+        adapter = new SmartChangeAdapter(getActivity().getApplicationContext());
         adapter.setOnChangeClickListener(mOnItemClickListener);
-        recycle.addOnScrollListener(mOnScrollListener);
         //recycle.setAdapter(adapter);
         recycle.removeAllViews();
         recycle.setAdapter(adapter);
     }
 
-    private ChangeAdapter.OnChangeClickListener mOnItemClickListener = new ChangeAdapter.OnChangeClickListener() {
+    private SmartChangeAdapter.OnChangeClickListener mOnItemClickListener = new SmartChangeAdapter.OnChangeClickListener() {
         @Override
         public void onItemClick(View view, int position) {
-            position = position - 1; //配对headerView
             Log.v("wwwwwwww", adapter.getItem(position) + "---true---" + position + "===" + adapter.getItem(position).getList().get(position % 20).getId());
             if (mData.size() <= 0) {
                 return;
@@ -91,70 +135,6 @@ class AllChangeFragment extends Fragment implements ChangeView {
             startActivity(intent);
         }
     };
-    private int lastVisibleItem;
-    private RecyclerView.OnScrollListener mOnScrollListener = new RecyclerView.OnScrollListener() {
-
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            super.onScrolled(recyclerView, dx, dy);
-            lastVisibleItem = mLayoutManager.findLastVisibleItemPosition();//可见的最后一个item
-        }
-
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-            Log.v("zzzzzzzzz", adapter.getItemCount() + "---" + (lastVisibleItem + 1) + "---" + (newState == RecyclerView.SCROLL_STATE_IDLE) + "===" + (lastVisibleItem + 1 == adapter.getItemCount()) + "-------?-----" + adapter.isShowFooter());
-            super.onScrollStateChanged(recyclerView, newState);
-            if (newState == RecyclerView.SCROLL_STATE_IDLE
-                    && lastVisibleItem + 1 == adapter.getItemCount()
-                    && adapter.isShowFooter() && lastVisibleItem - 1 > 8) {//加载判断条件 手指离开屏幕 到了footeritem
-                page++;
-                Log.v("zzzzzzzzz", "-------2-------" + page);
-                changePresenter.visitProjects(getActivity(), TYPE, changeBean, page);
-                Log.v("yyyy", "***onScrollStateChanged******");
-            }
-        }
-    };
-
-    public void initSwipeRefreshLayout() {
-        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                refresh.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (mDataAll != null) {
-                            mDataAll.clear();
-                        }
-                        if (mData != null) {
-                            mData.clear();
-                        }
-                        refresh.setRefreshing(false);
-                        page = 0;
-                        changePresenter.visitProjects(getActivity(), TYPE, changeBean, page);//请求刷新
-                    }
-                }, 2000);
-            }
-        });
-        refresh.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-    }
-
-    private void initHeader() {
-        //渲染header布局
-        ConstraintLayout h = new ConstraintLayout(getActivity());
-        ConstraintLayout.LayoutParams layoutParam = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dp2px(1.0f));
-        layoutParam.setMargins(0, 0, 0, 2);
-        h.setLayoutParams(layoutParam);
-        adapter.setHeaderView(h);
-    }
-
-    private int dp2px(float v) {
-        DisplayMetrics dm = getResources().getDisplayMetrics();
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, v, dm);
-    }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -170,8 +150,7 @@ class AllChangeFragment extends Fragment implements ChangeView {
             @Override
             public void run() {
                 if (changeBeans == null || changeBeans.size() == 0) {
-                    //AppUtils.showToast(getActivity(), getResources().getString(R.string.no_more));
-                    adapter.hasMore(false);//显示没有更多
+                    AppUtils.showToast(getActivity(), "数据全部加载完毕");
                     return;
                 }
                 if (mData == null && mDataAll == null) {
@@ -188,9 +167,6 @@ class AllChangeFragment extends Fragment implements ChangeView {
                     }
                     Log.v("zzzzzzzzz", "----4---------" + mData.size());
                     adapter.setmDate(mData);
-                    if (mDataAll.size() < 8) {
-                        adapter.hasMore(false);
-                    }
                 } else {
                     if (mDataAll != null) {
                         //加载更多
@@ -203,7 +179,6 @@ class AllChangeFragment extends Fragment implements ChangeView {
                         }
                     }
                 }
-                adapter.isShowFooter(true);
             }
         });
     }
---
+++ app/src/main/java/com/mango/leo/dcom/change/adapter/SmartChangeAdapter.java
@@ -0,0 +1,135 @@
+package com.mango.leo.dcom.change.adapter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mango.leo.dcom.R;
+import com.mango.leo.dcom.change.bean.ListChangeBean;
+import com.mango.leo.dcom.util.DateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 2018/5/11.
+ */
+
+public class SmartChangeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private OnChangeClickListener mOnChangeClickListener;//自注册的接口给调用者用于点击逻辑
+    private List<ListChangeBean> mData = new ArrayList<>();
+    public static final int TYPE_ITEM = 0;
+    private Handler mHandler = new Handler(Looper.getMainLooper()); //获取主线程的Handler
+
+    public void setmDate(List<ListChangeBean> data) {
+        this.mData = data;
+        this.notifyDataSetChanged();
+    }
+
+    public void reMove() {
+        List<ListChangeBean> m = new ArrayList<ListChangeBean>();
+        this.mData = m;
+        this.notifyDataSetChanged();
+    }
+
+
+    /**
+     * 添加列表项     * @param item
+     */
+    public void addItem(ListChangeBean bean) {
+        if (mData != null) {
+            mData.add(bean);
+        }
+        this.notifyDataSetChanged();
+    }
+    public SmartChangeAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+            ItemViewHolder vh = new ItemViewHolder(v);
+            return vh;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+            return TYPE_ITEM;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        final int pos = getRealPosition(holder);
+            if (((ItemViewHolder) holder) != null && mData.get(pos).getList() != null) {
+                Log.v("yyyyy", "====pos======" + pos % 20);//
+                ((ItemViewHolder) holder).textView_title.setText(mData.get(pos).getList().get(pos % 20).getTitle());
+                ((ItemViewHolder) holder).textView_time.setText(DateUtil.getDateToString(mData.get(pos).getList().get(pos % 20).getCreatedOn(),"yyyy-MM-dd HH:mm:ss"));
+                if (mData.get(pos).getList().get(pos % 20).getStage() == 0){
+                    ((ItemViewHolder) holder).textView_stage.setText("未提交");
+                }else if (mData.get(pos).getList().get(pos % 20).getStage() == 1){
+                    ((ItemViewHolder) holder).textView_stage.setText("已提交");
+                }
+            }
+    }
+
+    private int getRealPosition(RecyclerView.ViewHolder holder) {
+        int position = holder.getLayoutPosition();
+        return  position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void setOnChangeClickListener(OnChangeClickListener onItemnewsClickListener) {
+        this.mOnChangeClickListener = onItemnewsClickListener;
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView footTv;
+
+        public FooterViewHolder(View view) {
+            super(view);
+            footTv = (TextView) itemView.findViewById(R.id.more_data_msg);
+        }
+    }
+
+    public ListChangeBean getItem(int position) {
+        return mData == null ? null : mData.get(position);
+    }
+
+    public interface OnChangeClickListener {
+        public void onItemClick(View view, int position);
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView textView_title, textView_time, textView_stage;
+
+        public ItemViewHolder(View v) {
+            super(v);
+            textView_title = (TextView) v.findViewById(R.id.textView_title);
+            textView_time = (TextView) v.findViewById(R.id.textView_time);
+            textView_stage = (TextView) v.findViewById(R.id.textView_stage);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mOnChangeClickListener != null) {
+                mOnChangeClickListener.onItemClick(view, this.getLayoutPosition());
+            }
+        }
+    }
+}
